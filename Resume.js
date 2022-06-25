b2d8a60b412e2f4546ce1133d6e10dce1ccc05ae
VELLIYANGIRI RAJAGAESH <57614171+velliyangiri1996@users.noreply.github.com>
2022-06-25 20:24:38 +0530

    Here I upload the second task code files

---
+++ Resume.js
@@ -0,0 +1,35 @@
+let data = [
+    {
+    "NAME":"VELLIYANGIRI",
+    "PH.NO":"9876543210",
+    "MAIL_ID":"giri@gmail.com"
+    },
+      
+    {
+    "SUMMARY":"Three years of experience as Software Engineer with a skills in Machine Learning and Deep Learning with full-stack projects and building interactive ML/DL/NLP/ based web applications"
+    },
+
+    {
+    "2019-July to 2021-Jan": "Working as a Project Engineer at NOVITECH R&D Pvt,Ltd.",
+    "2021-March to 2021-Oct": "Working as a Project Engineer at ELK Education Consultant Pvt,Ltd.",
+    "2021-Nov to Present": "Working as a Project Engineer at Impiger Technologys Pvt,Ltd."
+    },
+
+    {
+    "SKILLS":"Machine learning, Deep learning, NLP, Python, HTML, CSS, JS, Image processing, CV, Chat-Bot, SQL"
+    },
+
+    {
+    "Project":"Heart Diseases Prediction using ML and DL and CV"
+    },
+
+    {
+    "EDUCATION":"BE-ECE"
+    },
+
+    {
+    "ADDRESS":"ERODE"
+    }]
+    for ( var i in data){
+    console.log(data[i])
+    }
---
+++ loopings.js
@@ -0,0 +1,105 @@
+let array = [
+    {
+      "id": "1",
+      "name": "Lura Senger",
+      "email": "Xander_Collier@yahoo.com"
+    },
+    {
+      "id": "2",
+      "name": "Wilburn Weber",
+      "email": "Bennett_Kreiger11@yahoo.com"
+    },
+    {
+      "id": "3",
+      "name": "Tyrique Hahn",
+      "email": "Juston.Altenwerth@yahoo.com"
+    },
+    {
+      "id": "4",
+      "name": "Lenny Bailey",
+      "email": "Guiseppe_Hegmann@yahoo.com"
+    },
+    {
+      "id": "5",
+      "name": "Vladimir Keeling",
+      "email": "Louisa_Walsh18@hotmail.com"
+    },
+    {
+      "id": "6",
+      "name": "Kellie Crona",
+      "email": "Chandler_Abernathy@yahoo.com"
+    },
+    {
+      "id": "7",
+      "name": "Carolina White",
+      "email": "Royal50@hotmail.com"
+    },
+    {
+      "id": "8",
+      "name": "Alfredo Conn",
+      "email": "Clarabelle34@hotmail.com"
+    },
+    {
+      "id": "9",
+      "name": "Stan Shanahan",
+      "email": "Lamar.McClure@hotmail.com"
+    },
+    {
+      "id": "10",
+      "name": "Marvin Fay",
+      "email": "Osbaldo58@hotmail.com"
+    },
+    {
+      "id": "11",
+      "name": "Torrance Rau",
+      "email": "Orin45@gmail.com"
+    },
+    {
+      "id": "12",
+      "name": "Harold Gutmann MD",
+      "email": "Alyce.Stracke37@yahoo.com"
+    },
+    {
+      "id": "13",
+      "name": "Taryn Torphy",
+      "email": "Dean_Breitenberg71@hotmail.com"
+    },
+    {
+      "id": "14",
+      "name": "Bryana Lang",
+      "email": "Tatum.Ullrich@yahoo.com"
+    },
+    {
+      "id": "15",
+      "name": "Nyasia Green DDS",
+      "email": "Dino83@gmail.com"
+    }]
+   
+    // FOR - IN LOOP
+    console.log("FOR - IN LOOP")
+ 
+    for ( var i in array){
+        console.log(i,array[i])
+    }
+    // FOR- LOOP
+    console.log("FOR LOOP")
+
+    for (var i=0; i<5; i++){
+        console.log(array[i])
+    }
+
+    // FOR - OF LOOP
+    console.log("FOR - OF LOOP")
+    
+    for ( var i of array){
+        console.log(i)
+    }
+
+    // FOR - EACH LOOP
+    console.log("FOR- EACH LOOP")
+    
+    array.forEach(myFunction);
+    function myFunction(out) {
+    console.log(out)
+}
+
